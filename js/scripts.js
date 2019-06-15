1854680145de853e3d6fb32c0ae752e51c31b537
Johnonyango <j.yayah7@gmail.com>
2019-06-15 05:34:15 +0300

    modified index.html file

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+// var sizes = ["large", "medium", "small"]
+// var crust = ["thin", "flatBread","thick""cripsy","Stuffed", "glutenFree"]
+// var toppings = ["pepperoni","sausage","bacon", "extraCheese"]
+
+$("document").ready(function(){
+  $(".btn").click(function(){
+  alert("Enter your location here")  
+  });
+});

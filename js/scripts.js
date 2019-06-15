57023f4a4433f768afaa699a5dbb014feef4c695
Johnonyango <j.yayah7@gmail.com>
2019-06-15 12:04:08 +0300

    several files modified

--- js/scripts.js
+++ js/scripts.js
@@ -2,8 +2,23 @@
 // var crust = ["thin", "flatBread","thick""cripsy","Stuffed", "glutenFree"]
 // var toppings = ["pepperoni","sausage","bacon", "extraCheese"]
 
-$("document").ready(function(){
-  $(".btn").click(function(){
-  alert("Enter your location here")  
-  });
+// $("document").ready(function(){
+//   $(".btn").click(function(){
+//   alert("Enter your location here")  
+//   });
+// });
+
+document.getElementById("#pizza-size");
+document.getElementById("#toppings");
+document.getElementById("#crust");
+document.getElementById("#deliver");
+document.getElementById("#charge");
+document.getElementById("#place");
+document.getElementById("#remove");
+
+$(document).ready(function(){
+  $("")
 });
+
+
+

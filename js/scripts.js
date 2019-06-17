ebe71016a8e04b6b3b2d8c67fde3dd0057dbc36b
Johnonyango <j.yayah7@gmail.com>
2019-06-17 10:01:00 +0300

    modified scripts.js file

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,45 @@
-function totalPrice(pizzaSize,toppings,crust,deliver) {
-  var pizzaSize =parseFloat(document.getElementById("pizzaSize").value);
-  var toppings =parseFloat(document.getElementById("toppings").value);
-  var crust =parseFloat(document.getElementById("crust").value);
-  var deliver =parseFloat(document.getElementById("deliver").value);
+// function totalPrice(pizzaSize,toppings,crust,deliver) {
+//   var pizzaSize =parseFloat(document.getElementById("pizzaSize").value);
+//   var toppings =parseFloat(document.getElementById("toppings").value);
+//   var crust =parseFloat(document.getElementById("crust").value);
+//   var deliver =parseFloat(document.getElementById("deliver").value);
 
-  function PizzaSize(large,medium,small){
-    document.this.large = 35000
-    document.this.medium = 2500
-    document.this.small =1500
+  function Charge(PizzaSize,Crust,Toppings,Deliver) {
+    function PizzaSize(large,medium,small){
+      this.large = 35000;
+      this.medium = 2500;
+      this.small =1500;
+      }
+      function Crust(Cripsy,Stuffed,Gluttenfree){
+      this.Cripsy = 500;
+      this.Stuffed = 850;
+      this.Gluttenfree = 1200;
+      }
+      function Toppings(Pepperoni,Sausage,Bacon){
+      this.Pepperoni = 500;
+      this.Sausage = 850;
+      this.Bacon = 1200;
+      }
+      function Deliver(delivery,nodelivery){
+      this.delivery = 500;
+      this.nodelivery = 0;
+      }
+      Charge.prototype.totalCharge = function() {
+        return this.PizzaSize + this.Crust + this.Toppings + this.Deliver;
   }
-  function Crust(Cripsy,Stuffed,Glutten-free){
-    document.this.Cripsy = 500
-    document.this.Stuffed = 850
-    document.this.small = 1200
-  }
-  function Toppings(Pepperoni,Sausage,Bacon){
-    document.this.Pepperoni = 500
-    document.this.Sausage = 850
-    document.this.Bacon = 1200
-  }
-  function Deliver(delivery,nodelivery){
-    document.this.delivery = 500
-    document.this.nodelivery = 0
-  }
+  
 
-
-  return pizzaSize + toppings +crust + deliver
-}
+  $(document).ready(function() {
+    $("#charge").submit(function(event) {
+      event.preventDefault();
+  
+      var inputtedPizzaSize = $("#pizzaSize").val();
+      var inputtedToppings = $("#toppings").val();
+      var inputtedCrust = $("#crust").val();
+      var inputtedDeliver = $("#deliver").val();
+  
+      var totalCharge = new Charge(inputtedPizzaSize, inputtedToppings,inputtedCrust,inputtedDeliver);
+  
+      $("ul#contacts").append(class = "label" + newContact.totalCharge());
+    });
+  });

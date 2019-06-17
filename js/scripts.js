5fe19ae922e679dbbf0cbe73dbdb4fb92a3062d3
Johnonyango <j.yayah7@gmail.com>
2019-06-17 08:01:11 +0300

    modified scripts.js file

--- js/scripts.js
+++ js/scripts.js
@@ -4,5 +4,26 @@ function totalPrice(pizzaSize,toppings,crust,deliver) {
   var crust =parseFloat(document.getElementById("crust").value);
   var deliver =parseFloat(document.getElementById("deliver").value);
 
+  function PizzaSize(large,medium,small){
+    document.this.large = 35000
+    document.this.medium = 2500
+    document.this.small =1500
+  }
+  function Crust(Cripsy,Stuffed,Glutten-free){
+    document.this.Cripsy = 500
+    document.this.Stuffed = 850
+    document.this.small = 1200
+  }
+  function Toppings(Pepperoni,Sausage,Bacon){
+    document.this.Pepperoni = 500
+    document.this.Sausage = 850
+    document.this.Bacon = 1200
+  }
+  function Deliver(delivery,nodelivery){
+    document.this.delivery = 500
+    document.this.nodelivery = 0
+  }
+
+
   return pizzaSize + toppings +crust + deliver
 }
